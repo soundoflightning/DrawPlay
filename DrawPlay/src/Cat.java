679d77eb7142530c91295f47ac8c5da1fce62c08
Thunder Cowan <thundercowan@mymail.mines.edu>
2019-10-02 20:43:06 -0600

    Changed the mouth size back, increased head dimensioin further.

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 200;
+	private static final int HEAD_DIMENSION = 250;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -14,7 +14,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 400;
+	private static final int MOUTH_HEIGHT = 40;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
